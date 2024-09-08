b9c0abbaafae90a95f4dac522814cc54513cb98e
Mohamad <mo.rajbi@gmail.com>
2024-09-08 13:44:17 +0330

    Initial commit

---
+++ website/app/routes/_index.tsx
@@ -0,0 +1,59 @@
+import type { MetaFunction } from "@remix-run/node";
+import * as stylex from "@stylexjs/stylex";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 {...stylex.props(styles.root)}>Welcome to Inline</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/quickstart"
+            rel="noreferrer"
+          >
+            5m Quick Start
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/tutorial"
+            rel="noreferrer"
+          >
+            30m Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer"
+          >
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+
+const styles = stylex.create({
+  root: {
+    fontSize: 42,
+    color: "blue",
+    fontWeight: "400",
+  },
+});
+
