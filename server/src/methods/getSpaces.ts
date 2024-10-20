32519ce02a93d3c132be2a58b1c0fad66a52da04
Dena Sohrabi <denasohrabi62@gmail.com>
2024-10-20 22:39:54 +0330

    Revert "add chats to getSpaces"
    This reverts commit ea804637b7a14494bb77058f32827aaf06d58da2.

--- server/src/methods/getSpaces.ts
+++ server/src/methods/getSpaces.ts
@@ -1,10 +1,8 @@
 import { db } from "@in/server/db"
 import { eq } from "drizzle-orm"
 import {
-  chats,
   members,
   spaces,
-  type DbChat,
   type DbMember,
   type DbSpace,
 } from "@in/server/db/schema"
@@ -12,10 +10,8 @@ import { ErrorCodes, InlineError } from "@in/server/types/errors"
 import { Log } from "@in/server/utils/log"
 import { type Static, Type } from "@sinclair/typebox"
 import {
-  encodeChatInfo,
   encodeMemberInfo,
   encodeSpaceInfo,
-  TChatInfo,
   TMemberInfo,
   TSpaceInfo,
 } from "@in/server/models"
@@ -31,20 +27,17 @@ type Context = {
 type RawOutput = {
   spaces: DbSpace[]
   members: DbMember[]
-  chats: DbChat[]
 }
 
 export const Response = Type.Object({
   spaces: Type.Array(TSpaceInfo),
   members: Type.Array(TMemberInfo),
-  chats: Type.Array(TChatInfo),
 })
 
 export const encode = (output: RawOutput): Static<typeof Response> => {
   return {
     spaces: output.spaces.map(encodeSpaceInfo),
     members: output.members.map(encodeMemberInfo),
-    chats: output.chats.map(encodeChatInfo),
   }
 }
 
@@ -58,14 +51,15 @@ export const handler = async (
       .from(members)
       .where(eq(members.userId, context.currentUserId))
       .innerJoin(spaces, eq(members.spaceId, spaces.id))
-      .innerJoin(chats, eq(members.spaceId, chats.spaceId))
+
     return {
       spaces: result.map((r) => r.spaces),
       members: result.map((r) => r.members),
-      chats: result.map((r) => r.chats),
     }
   } catch (error) {
     Log.shared.error("Failed to send email code", error)
     throw new InlineError(ErrorCodes.SERVER_ERROR, "Failed to send email code")
   }
 }
+
+/// HELPER FUNCTIONS ///
