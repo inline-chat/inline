0d47bb50fdde0c2e8d461707cc457477d85131e8
Dena Sohrabi <denasohrabi62@gmail.com>
2024-10-24 15:06:52 +0330

    update getUser

--- server/src/methods/getUser.ts
+++ server/src/methods/getUser.ts
@@ -8,7 +8,7 @@ import type { HandlerContext } from "@in/server/controllers/v1/helpers"
 import { encodeUserInfo, TUserInfo } from "../models"
 
 export const Input = Type.Object({
-  username: Type.String(),
+  id: Type.String(),
 })
 
 export const Response = Type.Object({
@@ -17,7 +17,11 @@ export const Response = Type.Object({
 
 export const handler = async (input: Static<typeof Input>, _: HandlerContext): Promise<Static<typeof Response>> => {
   try {
-    const result = await db.select().from(users).where(eq(users.username, input.username))
+    const id = parseInt(input.id)
+    if (isNaN(id)) {
+      throw new InlineError(ErrorCodes.INVALID_INPUT, "Invalid user ID")
+    }
+    const result = await db.select().from(users).where(eq(users.id, id))
     return { user: encodeUserInfo(result[0]) }
   } catch (error) {
     Log.shared.error("Failed to get user", error)
