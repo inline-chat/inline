231988e8306928f791f86fcdb043eb66b8870705
Mohamad <mo.rajbi@gmail.com>
2024-11-12 18:39:27 +0330

    New API root

--- server/src/buildEnv.ts
+++ server/src/buildEnv.ts
@@ -7,24 +7,20 @@ export const version = process.env.VERSION || (!isProd ? (await import("../packa
 export const gitCommitHash =
   process.env.GIT_COMMIT_HASH || (!isProd ? (await $`git rev-parse HEAD`.text()).trim().slice(0, 7) : "N/A")
 export const buildDate = process.env.BUILD_DATE || new Date().toISOString()
-
 export const relativeBuildDate = () => {
-  let date = new Date(buildDate)
-  let diff = new Date().getTime() - date.getTime()
+  const date = new Date(buildDate)
+  const diff = new Date().getTime() - date.getTime()
 
-  let seconds = Math.floor(diff / 1000) % 60
-  let minutes = Math.floor(diff / (1000 * 60)) % 60
-  let hours = Math.floor(diff / (1000 * 60 * 60)) % 24
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24))
+  const seconds = Math.floor(diff / 1000) % 60
+  const minutes = Math.floor(diff / (1000 * 60)) % 60
+  const hours = Math.floor(diff / (1000 * 60 * 60)) % 24
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24))
 
-  return `${days}d ${hours}h ${minutes}m ${seconds}s ago`
-}
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      BUILD_DATE: string
-      GIT_COMMIT_HASH: string
-      VERSION: string
-    }
-  }
+  const parts = []
+  if (days > 0) parts.push(`${days}d`)
+  if (hours > 0) parts.push(`${hours}h`)
+  if (minutes > 0) parts.push(`${minutes}m`)
+  if (seconds > 0) parts.push(`${seconds}s`)
+
+  return parts.length > 0 ? `${parts.join(" ")} ago` : "just now"
 }
--- server/src/controllers/root.ts
+++ server/src/controllers/root.ts
@@ -1,9 +1,74 @@
 import { Elysia } from "elysia"
 import { setup } from "@in/server/setup"
 import { gitCommitHash, relativeBuildDate, version } from "@in/server/buildEnv"
+import { html } from "@elysiajs/html"
 
 export const root = new Elysia()
   .use(setup)
+  .use(html())
   // DO NOT MODIFY THIS INITIAL PART OF MESSAGE
   // THIS IS MATCHED IN UPTIME MONITOR
-  .get("/", () => `🚧 inline server is running • v${version} • deployed ${relativeBuildDate()} • ${gitCommitHash}`)
+  .get("/", () => {
+    let title = `🚧 inline server is running`
+    let subtitle = `v${version} • deployed ${relativeBuildDate()} • ${gitCommitHash}`
+    let html = `
+      <html>
+        <head>
+          <title>~/dev/inline</title>
+          <link rel="icon" href="https://inline.chat/favicon.ico" />
+          <style>
+          html {
+              padding: 0;
+              margin: 0;
+              height: 100%;
+              background-color: #111;
+              background-image: linear-gradient(180deg, #111, #333);
+            }
+            body {
+              font-family: monospace;
+              font-size: 16px;
+              line-height: 1.5;
+              
+              color: #eee;
+              padding: 20px;
+              height: 100%;
+            }
+            h1, h2 {
+              margin: 0;
+              font-size: 16px;
+            }
+            p {
+              margin: 0;
+              margin-top: 4px;
+              color: #ccc;
+            }
+            a {
+              display: inline-block;
+              color: #ccc;
+              text-decoration: none;
+              border-radius: 4px;
+              margin-right: 2px;
+            }
+            footer {
+              border-top: 1px solid #555;
+              padding-top: 20px;
+              font-size: 16px;
+              max-width: 500px;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>${title}</h1>
+          <p>${subtitle}</p>
+          <footer style="margin-top: 20px;">
+          <a href="https://inline.chat">inline.chat</a>
+          <a><span>•</span></a>
+          <a href="https://status.inline.chat">is inline down?</a>
+          <a><span>•</span></a>
+          <a href="mailto:hi@inline.chat">hi@inline.chat</a>
+          </footer>
+        </body>
+      </html>
+    `
+    return html
+  })
