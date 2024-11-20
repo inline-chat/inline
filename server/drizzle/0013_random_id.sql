049271b1524b49e3c885f61b42fd3e2ad7610652
Mohamad <mo.rajbi@gmail.com>
2024-11-20 18:35:26 +0330

    Undo random_id migration

--- server/drizzle/0013_random_id.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "messages" DROP CONSTRAINT "messages_peer_user_id_users_id_fk";
---> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "random_id" bigint;--> statement-breakpoint
-ALTER TABLE "messages" DROP COLUMN IF EXISTS "peer_user_id";--> statement-breakpoint
-ALTER TABLE "messages" ADD CONSTRAINT "random_id_per_sender_unique" UNIQUE("random_id","from_id");
