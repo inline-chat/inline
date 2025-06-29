81a8c039a4ce84b3d5aa8614ce63647423012e76
Mohamad <mo.rajbi@gmail.com>
2025-06-30 02:15:18 +0330

    apple: improve realtime API

--- apple/InlineKit/Sources/RealtimeAPI/MsgQueue.swift
+++ apple/InlineKit/Sources/RealtimeAPI/MsgQueue.swift
@@ -3,58 +3,45 @@ import Foundation
 import InlineProtocol
 import Logger
 
-package final class MsgQueue: @unchecked Sendable {
+package final actor MsgQueue: Sendable {
   private let log = Log.scoped("Realtime_MsgQueue")
-  private let lock = NSLock()
   private var _queue: [ClientMessage] = []
   private var _inFlight: [UInt64: ClientMessage] = [:]
 
   public func push(message: ClientMessage) {
-    lock.withLock {
-      _queue.append(message)
-      log.debug("Pushed message \(message.id), queue size: \(_queue.count)")
-    }
+    _queue.append(message)
+    log.debug("Pushed message \(message.id), queue size: \(_queue.count)")
   }
 
   public func next() -> ClientMessage? {
-    lock.withLock {
-      guard !_queue.isEmpty else { return nil }
-      let message = _queue.removeFirst()
-      _inFlight[message.id] = message
-      log.debug("Next message \(message.id), queue size: \(_queue.count)")
-      return message
-    }
+    guard !_queue.isEmpty else { return nil }
+    let message = _queue.removeFirst()
+    _inFlight[message.id] = message
+    log.debug("Next message \(message.id), queue size: \(_queue.count)")
+    return message
   }
 
   public func requeue(_ message: ClientMessage) {
-    lock.withLock {
-      _queue.insert(message, at: 0)
-      _inFlight.removeValue(forKey: message.id)
-    }
+    _queue.insert(message, at: 0)
+    _inFlight.removeValue(forKey: message.id)
   }
 
   // Add to MsgQueue:
   public func requeueAllInFlight() {
-    lock.withLock {
-      _queue = _inFlight.values + _queue
-      _inFlight.removeAll()
-    }
+    _queue = Array(_inFlight.values) + _queue
+    _inFlight.removeAll()
   }
 
   public func remove(msgId: UInt64) {
-    lock.withLock {
-      _inFlight.removeValue(forKey: msgId)
-    }
+    _inFlight.removeValue(forKey: msgId)
   }
 
   public var isEmpty: Bool {
-    lock.withLock { _queue.isEmpty }
+    _queue.isEmpty
   }
-  
+
   public func removeAll() {
-    lock.withLock {
-      _queue.removeAll()
-      _inFlight.removeAll()
-    }
+    _queue.removeAll()
+    _inFlight.removeAll()
   }
 }
