2c8ae38d84629e0449dde9e7c3299a22e106e61b
Mohamad <mo.rajbi@gmail.com>
2025-07-15 01:10:57 +0330

    api: relayout notification

--- server/src/modules/cache/userNames.ts
+++ server/src/modules/cache/userNames.ts
@@ -8,12 +8,18 @@ export type UserName = {
   lastName: string | null
   username: string | null
   email: string | null
+  phone: string | null
   timeZone: string | null
   cacheDate: number
 }
 
 const cachedUserNames = new Map<number, UserName>()
 const cacheValidTime = 120 * 1000 // 120s
+
+export const UserNamesCache = {
+  getCachedUserName,
+  getDisplayName,
+}
 
 export async function getCachedUserName(userId: number): Promise<UserName | undefined> {
   let cached = cachedUserNames.get(userId)
@@ -38,7 +44,8 @@ export async function getCachedUserName(userId: number): Promise<UserName | unde
     firstName: user.firstName,
     lastName: user.lastName,
     username: user.username,
-    email: user.email,
+    email: user.emailVerified ? user.email : null,
+    phone: user.phoneVerified ? user.phoneNumber : null,
     cacheDate: Date.now(),
     timeZone: user.timeZone,
   }
@@ -46,4 +53,33 @@ export async function getCachedUserName(userId: number): Promise<UserName | unde
   cachedUserNames.set(userId, userName)
 
   return userName
+}
+
+/**
+ * Get the display name of a user
+ * @param userName - The user name cache object
+ * @returns The display name
+ */
+function getDisplayName(userName: UserName): string | null {
+  if (userName.firstName) {
+    return userName.firstName
+  }
+
+  if (userName.lastName) {
+    return userName.lastName
+  }
+
+  if (userName.username) {
+    return userName.username
+  }
+
+  if (userName.email) {
+    return userName.email
+  }
+
+  if (userName.phone) {
+    return userName.phone
+  }
+
+  return null
 }
