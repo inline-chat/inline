eea8e9201bb092ec1856fae3cd824babc793e23f
Dena Sohrabi <87666169+dena-sohrabi@users.noreply.github.com>
2025-07-29 19:06:40 +0330

    added context menu view

--- apple/InlineIOS/Utils/ContextMenuManager.swift
+++ apple/InlineIOS/Utils/ContextMenuManager.swift
@@ -14,6 +14,7 @@ final class ContextMenuManager {
   }()
 
   private weak var previousKeyWindow: UIWindow?
+  private weak var currentMenu: ContextMenuView?
 
   func show(for gesture: UIGestureRecognizer, message: FullMessage, spaceId: Int64) {
     guard let sourceView = gesture.view else { return }
@@ -23,7 +24,13 @@ final class ContextMenuManager {
     let rootVC = UIViewController()
     rootVC.view.backgroundColor = .clear
 
-    let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .systemMaterial))
+    let dimmingView = UIView(frame: rootVC.view.bounds)
+    dimmingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    dimmingView.backgroundColor = UIColor.black.withAlphaComponent(0.25)
+    dimmingView.alpha = 0
+    rootVC.view.addSubview(dimmingView)
+
+    let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
     blurView.frame = rootVC.view.bounds
     blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     rootVC.view.addSubview(blurView)
@@ -37,9 +44,116 @@ final class ContextMenuManager {
     scrollView.alwaysBounceVertical = true
     scrollView.contentInsetAdjustmentBehavior = .never
 
+    let menuElements: [ContextMenuElement] = [
+      .item(ContextMenuItem(
+        title: "Reply",
+        icon: UIImage(systemName: "arrowshape.turn.up.left"),
+        action: { [weak self] in
+          print("Reply tapped")
+          self?.hide()
+        }
+      )),
+      .separator,
+      .item(ContextMenuItem(
+        title: "Copy",
+        icon: UIImage(systemName: "doc.on.doc"),
+        action: { [weak self] in
+          print("Copy tapped")
+          self?.hide()
+        }
+      )),
+      .separator,
+      .item(ContextMenuItem(
+        title: "Forward",
+        icon: UIImage(systemName: "arrowshape.turn.up.right"),
+        action: { [weak self] in
+          print("Forward tapped")
+          self?.hide()
+        }
+      )),
+      .separator,
+      .item(ContextMenuItem(
+        title: "Delete",
+        icon: UIImage(systemName: "trash"),
+        isDestructive: true,
+        action: { [weak self] in
+          print("Delete tapped")
+          self?.hide()
+        }
+      )),
+    ]
+
+    let contextMenu = ContextMenuView(elements: menuElements)
+    contextMenu.translatesAutoresizingMaskIntoConstraints = false
+    currentMenu = contextMenu
+
+    // Prepare for animated presentation
+    contextMenu.prepareForPresentation()
+
+    // Add context menu to get its size
+    scrollView.addSubview(contextMenu)
+
+    // Get menu size by laying it out
+    contextMenu.setNeedsLayout()
+    contextMenu.layoutIfNeeded()
+    let menuSize = contextMenu.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+
+    // Calculate available space and positioning
+    let screenBounds = UIScreen.main.bounds
+    let sourceFrame = sourceView.convert(sourceView.bounds, to: nil)
+    let menuSpacing: CGFloat = 20
+    let safeAreaBottom = rootVC.view.safeAreaInsets.bottom
+
+    let availableSpaceBelow = screenBounds.height - sourceFrame.maxY - safeAreaBottom - menuSpacing
+    let menuFitsBelow = availableSpaceBelow >= menuSize.height
+
     let messageView = UIMessageView(fullMessage: message, spaceId: spaceId)
-    messageView.frame = sourceView.convert(sourceView.bounds, to: scrollView)
+    messageView.translatesAutoresizingMaskIntoConstraints = false
     scrollView.addSubview(messageView)
+
+    var messageTopConstraint: NSLayoutConstraint
+    var contextMenuTopConstraint: NSLayoutConstraint
+
+    if menuFitsBelow {
+      // Menu fits below - normal positioning
+      let originalFrame = sourceView.convert(sourceView.bounds, to: scrollView)
+      messageTopConstraint = messageView.topAnchor.constraint(
+        equalTo: scrollView.topAnchor,
+        constant: originalFrame.minY
+      )
+      contextMenuTopConstraint = contextMenu.topAnchor.constraint(
+        equalTo: messageView.bottomAnchor,
+        constant: menuSpacing
+      )
+    } else {
+      // Menu doesn't fit below - push message up
+      let requiredHeight = sourceFrame.height + menuSpacing + menuSize.height
+      let pushUpAmount = requiredHeight - availableSpaceBelow - sourceFrame.height
+      let adjustedY = max(rootVC.view.safeAreaInsets.top + 20, sourceFrame.minY - pushUpAmount)
+
+      messageTopConstraint = messageView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: adjustedY)
+      contextMenuTopConstraint = contextMenu.topAnchor.constraint(
+        equalTo: messageView.bottomAnchor,
+        constant: menuSpacing
+      )
+    }
+
+    NSLayoutConstraint.activate([
+      messageView.leadingAnchor.constraint(
+        equalTo: scrollView.leadingAnchor,
+        constant: sourceView.convert(sourceView.bounds, to: scrollView).minX
+      ),
+      messageView.widthAnchor.constraint(equalToConstant: sourceView.bounds.width),
+      messageView.heightAnchor.constraint(equalToConstant: sourceView.bounds.height),
+      messageTopConstraint,
+
+      contextMenu.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+      contextMenuTopConstraint,
+    ])
+
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTapped))
+    tapGesture.cancelsTouchesInView = false
+    rootVC.view.addGestureRecognizer(tapGesture)
 
     rootVC.view.addSubview(scrollView)
 
@@ -56,14 +170,44 @@ final class ContextMenuManager {
 
     overlayWindow.isHidden = false
     overlayWindow.makeKeyAndVisible()
+    UIView.animate(withDuration: 0.25) {
+      dimmingView.alpha = 1
+    }
+    contextMenu.animateIn(after: 0)
   }
 
   func hide() {
-    overlayWindow.isHidden = true
-    overlayWindow.windowScene = nil
-    overlayWindow.rootViewController = nil
     overlayWindow.resignKey()
+    if let rootVC = overlayWindow.rootViewController,
+       let dimmingView = rootVC.view.subviews
+       .first(where: { $0.backgroundColor?.cgColor.alpha ?? 0 > 0 && $0 is UIView })
+    {
+      // Run both menu and dimming animations in parallel.
+      currentMenu?.animateOut()
 
-    previousKeyWindow?.makeKeyAndVisible()
+      UIView.animate(withDuration: 0.15, animations: {
+        dimmingView.alpha = 0
+      }) { _ in
+        self.overlayWindow.isHidden = true
+        self.overlayWindow.windowScene = nil
+        self.overlayWindow.rootViewController = nil
+        self.overlayWindow.resignKey()
+
+        self.previousKeyWindow?.makeKeyAndVisible()
+        self.currentMenu = nil
+      }
+    } else {
+      overlayWindow.isHidden = true
+      overlayWindow.windowScene = nil
+      overlayWindow.rootViewController = nil
+      overlayWindow.resignKey()
+
+      previousKeyWindow?.makeKeyAndVisible()
+      currentMenu = nil
+    }
+  }
+
+  @objc private func backgroundTapped() {
+    hide()
   }
 }
