1137ff6d6a38e0e51eac644638914ebb971b7333
Mohamad <mo.rajbi@gmail.com>
2025-07-31 23:25:41 +0330

    improve transport

--- apple/InlineKit/Sources/RealtimeV2/Transport/Transport.swift
+++ apple/InlineKit/Sources/RealtimeV2/Transport/Transport.swift
@@ -1,3 +1,4 @@
+import AsyncAlgorithms
 import Foundation
 import InlineProtocol
 
@@ -7,6 +8,9 @@ public enum TransportEvent: Sendable {
 
   /// The transport is fully connected and ready to send/receive messages.
   case connected
+
+  /// The transport is stopping. Due to a logout or fatal error during connection flow.
+  case stopping
 
   /// A message was received from the server.
   case message(ServerProtocolMessage)
@@ -18,7 +22,7 @@ public enum TransportError: Error {
 
 public protocol Transport: Sendable {
   /// Unified stream of life-cycle events and inbound server messages.
-  var events: AsyncStream<TransportEvent> { get }
+  var events: AsyncChannel<TransportEvent> { get }
 
   /// Connect (or reconnect) to the remote endpoint.  Idempotent.
   func start() async
@@ -31,4 +35,27 @@ public protocol Transport: Sendable {
   /// Send an encoded `ClientMessage` to the server.  Throws if the transport
   /// is not currently in the `.connected` state.
   func send(_ message: ClientMessage) async throws
+}
+
+// MARK: - Implementation Helpers
+
+extension Transport {
+  /// Restart the start connect after a delay
+  ///
+  /// Must be called if a fatal error occurs during connection flow.
+  func restart(retryDelay: TimeInterval = 2.0) async {
+    // Stop current transport
+    await stop()
+
+    // Wait for the specified delay
+    try? await Task.sleep(for: .seconds(retryDelay))
+
+    // Check if task was cancelled during sleep
+    guard !Task.isCancelled else {
+      return
+    }
+
+    // Start transport again
+    await start()
+  }
 }
