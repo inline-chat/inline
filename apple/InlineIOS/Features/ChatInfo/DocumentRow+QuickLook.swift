dbe9da785fc6c81f123034199feecc63f8243d04
Dena Sohrabi <87666169+dena-sohrabi@users.noreply.github.com>
2025-08-06 02:49:34 +0330

    ios: add download and clean up chat info

--- apple/InlineIOS/Features/ChatInfo/DocumentRow+QuickLook.swift
+++ apple/InlineIOS/Features/ChatInfo/DocumentRow+QuickLook.swift
@@ -1,8 +1,11 @@
 import QuickLook
 import SwiftUI
 
+/// A SwiftUI wrapper around `QLPreviewController` that previews a single local file.
 struct QuickLookView: UIViewControllerRepresentable {
   let url: URL
+
+  // MARK: - UIViewControllerRepresentable
 
   func makeUIViewController(context: Context) -> QLPreviewController {
     let controller = QLPreviewController()
@@ -12,34 +15,28 @@ struct QuickLookView: UIViewControllerRepresentable {
   }
 
   func updateUIViewController(_ uiViewController: QLPreviewController, context: Context) {
-    // No updates needed
+    // No dynamic updates required – file URL is immutable for the lifetime of the sheet.
   }
 
   func makeCoordinator() -> Coordinator {
     Coordinator(url: url)
   }
 
-  class Coordinator: NSObject, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
+  // MARK: - Coordinator
+
+  final class Coordinator: NSObject, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
     let url: URL
+    init(url: URL) { self.url = url }
 
-    init(url: URL) {
-      self.url = url
-    }
-
-    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
-      1
-    }
-
+    // QLPreviewControllerDataSource
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int { 1 }
     func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
       url as QLPreviewItem
     }
   }
 }
 
-// MARK: - Extensions
-
+// Convenience bridge for theme colours used elsewhere in the file-group.
 extension UIColor {
-  var color: Color {
-    Color(self)
-  }
+  var color: Color { Color(self) }
 }
