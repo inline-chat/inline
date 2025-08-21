b292faa0046e04f5e371ee882ecd44b7cbc82c3f
Mohamad <mo.rajbi@gmail.com>
2025-08-22 00:12:49 +0330

    apple/realtime2: migrate add reaction to it

--- apple/InlineKit/Sources/RealtimeV2/Errors.swift
+++ apple/InlineKit/Sources/RealtimeV2/Errors.swift
@@ -1,20 +1,19 @@
 import InlineProtocol
 
+public typealias TransactionError2 = TransactionError
 public enum TransactionError: Error {
   case rpcError(InlineProtocol.RpcError)
   case timeout
+  case invalid
   case cancelled
 }
 
 extension TransactionError {
   static func executionError(_ error: TransactionExecutionError) -> Self {
     switch error {
-      case .rpcError:
-        // Map to a generic RPC error since we don't have the specific RpcError details
-        .rpcError(InlineProtocol.RpcError())
       case .invalid:
         // Map invalid execution errors to cancelled since it represents a failed transaction state
-        .cancelled
+        .invalid
     }
   }
 }
