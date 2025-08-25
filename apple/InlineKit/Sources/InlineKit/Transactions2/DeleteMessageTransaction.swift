ca9812e7fef1e627b5fa50bca5e174086c3a478d
Mohamad <mo.rajbi@gmail.com>
2025-08-25 15:09:30 +0330

    apple/realtime: use context for codable

--- apple/InlineKit/Sources/InlineKit/Transactions2/DeleteMessageTransaction.swift
+++ apple/InlineKit/Sources/InlineKit/Transactions2/DeleteMessageTransaction.swift
@@ -6,26 +6,36 @@ import Logger
 import RealtimeV2
 
 public struct DeleteMessageTransaction: Transaction2 {
-  // Properties
   public var method: InlineProtocol.Method = .deleteMessages
-  public var input: InlineProtocol.RpcCall.OneOf_Input?
+  public var context: Context
 
-  // Private
+  public struct Context: Sendable, Codable {
+    public var messageIds: [Int64]
+    public var peerId: Peer
+    public var chatId: Int64
+  }
+
+  enum CodingKeys: String, CodingKey {
+    case context
+  }
+
   private var log = Log.scoped("Transactions/DeleteMessage")
-  private var messageIds: [Int64]
-  private var peerId: Peer
-  private var chatId: Int64
 
   public init(messageIds: [Int64], peerId: Peer, chatId: Int64) {
-    let filteredMessageIds = messageIds.filter { Int64($0) > 0 }
-    input = .deleteMessages(.with {
-      $0.peerID = peerId.toInputPeer()
-      $0.messageIds = filteredMessageIds
-    })
-    self.messageIds = filteredMessageIds
-    self.peerId = peerId
-    self.chatId = chatId
+    context = Context(messageIds: messageIds, peerId: peerId, chatId: chatId)
   }
+
+  public func input(from context: Context) -> InlineProtocol.RpcCall.OneOf_Input? {
+    .deleteMessages(.with {
+      $0.peerID = context.peerId.toInputPeer()
+      $0.messageIds = context.messageIds
+    })
+  }
+
+  // Computed
+  public var messageIds: [Int64] { context.messageIds }
+  public var peerId: Peer { context.peerId }
+  public var chatId: Int64 { context.chatId }
 
   // Methods
   public func optimistic() async {
@@ -51,42 +61,6 @@ public struct DeleteMessageTransaction: Transaction2 {
     }
 
     await Api.realtime.applyUpdates(response.updates)
-  }
-
-  public func failed(error: TransactionError2) async {
-    log.error("Failed to delete message", error: error)
-  }
-}
-
-// MARK: - Codable
-
-extension DeleteMessageTransaction: Codable {
-  enum CodingKeys: String, CodingKey {
-    case messageIds, peerId, chatId
-  }
-
-  public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(messageIds, forKey: .messageIds)
-    try container.encode(peerId, forKey: .peerId)
-    try container.encode(chatId, forKey: .chatId)
-  }
-
-  public init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-
-    messageIds = try container.decode([Int64].self, forKey: .messageIds)
-    peerId = try container.decode(Peer.self, forKey: .peerId)
-    chatId = try container.decode(Int64.self, forKey: .chatId)
-
-    // Set method
-    method = .deleteMessages
-
-    // Reconstruct Protocol Buffer input
-    input = .deleteMessages(.with {
-      $0.peerID = peerId.toInputPeer()
-      $0.messageIds = messageIds
-    })
   }
 }
 
