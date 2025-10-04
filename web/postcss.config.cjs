8763317785805f529334741187f329da3617fd5d
Mohamad <mo.rajbi@gmail.com>
2025-10-04 17:45:41 +0330

    move to tanstack start

---
+++ web/postcss.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    // "@stylexjs/postcss-plugin": {
+    //   include: [
+    //     "./src/**/*.{js,jsx,ts,tsx}",
+    //     "./**/*.{js,jsx,ts,tsx}",
+    //     "./*.{js,jsx,ts,tsx}",
+    //     // any other files that should be included
+    //     // this should include NPM dependencies that use StyleX
+    //   ],
+    //   useCSSLayers: true,
+    // },
+    // "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+}
