0581fcdc2dbea39bc26b25b05313f5a1fccf8204
Mohamad <mo.rajbi@gmail.com>
2025-10-04 17:45:41 +0330

    move to tanstack start

--- web/app/routes/feedback.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "react-router"
-
-export const loader = async () => {
-  return redirect("https://inlinehq.notion.site/14b361a8824f80bba76dc53046aa4efc?pvs=105")
-}
--- web/src/routes.ts
+++ web/src/routes.ts
@@ -5,10 +5,10 @@ export default [
   index("./routes/home.tsx", { id: "landing-home" }),
 
   // website public pages
-  route("privacy", "./routes/privacy.tsx"),
-  route("feedback", "./routes/feedback.tsx"),
-  route("docs", "./routes/docs.tsx"),
+  //route("privacy", "./routes/privacy.tsx"),
+  //route("feedback", "./routes/feedback.tsx"),
+  //route("docs", "./routes/docs.tsx"),
 
   // /home - alternative for loading landing page for authenticated users
-  route("home", "./routes/home.tsx"),
+  //route("home", "./routes/home.tsx"),
 ] satisfies RouteConfig
