6c1d9c6f1023e5528c85299d2a8100bd6d267ab9
Dena Sohrabi <87666169+dena-sohrabi@users.noreply.github.com>
2025-11-02 19:35:33 +0330

    ios: new progressive blur

---
+++ apple/InlineIOS/Features/Chat/ChatViewHeader.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct ChatViewHeader: View {
+  @Binding private var navBarHeight: CGFloat
+
+  init(navBarHeight: Binding<CGFloat>) {
+    _navBarHeight = navBarHeight
+  }
+
+  let theme = ThemeManager.shared.selected
+  var body: some View {
+//    LinearGradient(
+//      gradient: Gradient(colors: [
+//        Color(theme.backgroundColor).opacity(1),
+//        Color(theme.backgroundColor).opacity(0.0),
+//      ]),
+//      startPoint: .top,
+//      endPoint: .bottom
+//    )
+    VariableBlurView(maxBlurRadius: 2)
+      /// +28 to enhance the variant blur effect; it needs more space to cover the full navigation bar background
+//    .frame(height: 60)  was 38
+      .frame(height: navBarHeight + 12) // was 38
+       .contentShape(Rectangle())
+        .background(
+          LinearGradient(
+            gradient: Gradient(colors: [
+              ThemeManager.shared.backgroundColorSwiftUI.opacity(1),
+              ThemeManager.shared.backgroundColorSwiftUI.opacity(0.0),
+            ]),
+            startPoint: .top,
+            endPoint: .bottom
+          )
+        )
+
+      // Spacer()
+      .ignoresSafeArea(.all)
+      .allowsHitTesting(false)
+  }
+}
