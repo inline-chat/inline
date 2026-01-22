3dfb9dc81a4d86ae5730194c844fdb6bd075c4de
Mohamad <mo.rajbi@gmail.com>
2026-01-22 11:51:56 +0330

    macos: move app entry to main actor

--- apple/InlineMac/main.swift
+++ apple/InlineMac/main.swift
@@ -1,7 +1,12 @@
 import AppKit
 
-let app = NSApplication.shared
-let delegate = AppDelegate()
-app.delegate = delegate
-app.run()
-
+@main
+struct InlineMacMain {
+  @MainActor
+  static func main() {
+    let app = NSApplication.shared
+    let delegate = AppDelegate()
+    app.delegate = delegate
+    app.run()
+  }
+}
