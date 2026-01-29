f16cf0efd337cc98b7a23ce779c2124bc00a8237
Dena Sohrabi <87666169+dena-sohrabi@users.noreply.github.com>
2026-01-30 00:51:16 +0330

    server: revert db init changes

--- server/scripts/helpers/migrate-db.ts
+++ server/scripts/helpers/migrate-db.ts
@@ -3,9 +3,9 @@ import { migrate } from "drizzle-orm/postgres-js/migrator"
 import { drizzle } from "drizzle-orm/postgres-js"
 import { resolve } from "path"
 
+const DATABASE_URL = process.env["DATABASE_URL"] as string
 export const migrateDb = async () => {
-  const databaseUrl = process.env["DATABASE_URL"] as string
-  const migrationClient = postgres(databaseUrl, { max: 1 })
+  const migrationClient = postgres(DATABASE_URL, { max: 1 })
 
   // This will run migrations on the database, skipping the ones already applied
   await migrate(drizzle(migrationClient), {
