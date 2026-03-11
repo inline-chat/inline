4409b297f10df11ea776ec86206e595f7909da24
Dena Sohrabi <87666169+dena-sohrabi@users.noreply.github.com>
2026-03-11 17:35:14 +0330

    add inline x ios target

---
+++ apple/InlineXIOS/InlineXIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InlineXIOSApp.swift
+//  InlineXIOS
+//
+//  Created by Dena Sohrabi  on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct InlineXIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Main()
+        }
+    }
+}
