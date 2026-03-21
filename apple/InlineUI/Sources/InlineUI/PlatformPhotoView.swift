1f503f17e0293c4320eb7651a5361e147803c432
Mohammad Rajabi <mo.rajbi@gmail.com>
2026-03-22 00:21:49 +0330

    ios: fix embedded reply photo thumbnails

--- apple/InlineUI/Sources/InlineUI/PlatformPhotoView.swift
+++ apple/InlineUI/Sources/InlineUI/PlatformPhotoView.swift
@@ -31,7 +31,6 @@ public final class PlatformPhotoView: PlatformView {
       }
     }
   }
-
   public var showsTinyThumbnailBackground = false {
     didSet {
       updateTinyThumbnailBackground()
