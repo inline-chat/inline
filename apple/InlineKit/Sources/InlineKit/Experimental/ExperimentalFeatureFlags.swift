a9c3e7f2b1c374bcf6de33b6a083a3b94c81faa6
Mohammad Rajabi <mo.rajbi@gmail.com>
2026-04-08 01:39:13 +0330

    macos: add reply thread experimental toggle

--- apple/InlineKit/Sources/InlineKit/Experimental/ExperimentalFeatureFlags.swift
+++ apple/InlineKit/Sources/InlineKit/Experimental/ExperimentalFeatureFlags.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public enum ExperimentalFeatureFlags {
   public static let voiceMessagesKey = "experimental.voiceMessages"
+  public static let replyThreadMenuItemsKey = "experimental.replyThreadMenuItems"
 
   public static var voiceMessagesEnabled: Bool {
     get { isVoiceMessagesEnabled }
@@ -14,5 +15,18 @@ public enum ExperimentalFeatureFlags {
 
   public static func setVoiceMessagesEnabled(_ isEnabled: Bool) {
     UserDefaults.standard.set(isEnabled, forKey: voiceMessagesKey)
+  }
+
+  public static var replyThreadMenuItemsEnabled: Bool {
+    get { isReplyThreadMenuItemsEnabled }
+    set { setReplyThreadMenuItemsEnabled(newValue) }
+  }
+
+  public static var isReplyThreadMenuItemsEnabled: Bool {
+    UserDefaults.standard.bool(forKey: replyThreadMenuItemsKey)
+  }
+
+  public static func setReplyThreadMenuItemsEnabled(_ isEnabled: Bool) {
+    UserDefaults.standard.set(isEnabled, forKey: replyThreadMenuItemsKey)
   }
 }
