23d334734413a55e002b762c796d88c0a14badb2
Mohammad Rajabi <mo.rajbi@gmail.com>
2026-08-27 17:54:19 +0330

    ios: add experimental message view 2 renderer

---
+++ apple/InlineIOS/Features/Message/RichContentV2/RichBlockLayoutPlanV2.swift
@@ -0,0 +1,122 @@
+import InlineKit
+import UIKit
+
+enum RichBlockTextRoleV2: Hashable {
+  case paragraph
+  case heading(level: Int)
+  case footer
+  case disclosure(progress: Bool, expanded: Bool)
+  case listMarker
+}
+
+enum RichBlockRenderKindV2: Hashable {
+  case text
+  case listMarker
+  case code
+  case disclosure
+  case separator
+  case image
+  case album
+  case quote
+  case table
+}
+
+struct RichBlockLayoutPlanV2: Hashable {
+  struct TrailingTextLine: Hashable {
+    let usedWidth: CGFloat
+    let height: CGFloat
+    let isRTL: Bool
+  }
+
+  struct TextNode: Hashable {
+    let range: NSRange
+    let role: RichBlockTextRoleV2
+    let literal: String?
+    let isRTL: Bool
+  }
+
+  struct CodeNode: Hashable {
+    let range: NSRange
+    let language: String?
+    let gutterWidth: CGFloat
+    let lineCount: Int
+    let contentWidth: CGFloat
+  }
+
+  struct ImageNode: Hashable {
+    enum State: Hashable {
+      case pending
+      case ready(PhotoInfo)
+      case unavailable
+    }
+
+    let path: BlockContentPath
+    let frame: CGRect
+    let state: State
+  }
+
+  struct AlbumNode: Hashable {
+    let items: [ImageNode]
+    let contentWidth: CGFloat
+  }
+
+  struct QuoteNode: Hashable {
+    let isRTL: Bool
+  }
+
+  enum TableAlignment: Hashable {
+    case leading
+    case center
+    case trailing
+  }
+
+  struct TableNode: Hashable {
+    struct Cell: Hashable {
+      let range: NSRange
+      let frame: CGRect
+      let alignment: TableAlignment
+      let isHeader: Bool
+    }
+
+    let cells: [Cell]
+    let contentWidth: CGFloat
+    let isRTL: Bool
+  }
+
+  enum NodeKind: Hashable {
+    case text(TextNode)
+    case code(CodeNode)
+    case separator
+    case image(ImageNode)
+    case album(AlbumNode)
+    case quote(QuoteNode)
+    case table(TableNode)
+  }
+
+  struct Node: Hashable {
+    let path: BlockContentPath
+    var frame: CGRect
+    let kind: NodeKind
+
+    var reuseKind: RichBlockRenderKindV2 {
+      switch kind {
+        case let .text(text):
+          switch text.role {
+            case .listMarker: .listMarker
+            case .disclosure: .disclosure
+            default: .text
+          }
+        case .code: .code
+        case .separator: .separator
+        case .image: .image
+        case .album: .album
+        case .quote: .quote
+        case .table: .table
+      }
+    }
+  }
+
+  let size: CGSize
+  let nodes: [Node]
+  let trailingTextLine: TrailingTextLine?
+}
