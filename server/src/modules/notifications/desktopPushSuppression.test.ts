1bf40d0e1a6b61bbfa8901fdf680bcaa09b9c3df
Mohammad Rajabi <mo.rajbi@gmail.com>
2026-09-09 01:59:57 +0330

    server: shorten desktop push suppression

--- server/src/modules/notifications/desktopPushSuppression.test.ts
+++ server/src/modules/notifications/desktopPushSuppression.test.ts
@@ -39,7 +39,7 @@ describe("desktopPushSuppression", () => {
 
     await tracker.recordChatActivity({ userId: 1, sessionId: 1, chatId: 100 })
 
-    now += 29_999
+    now += 14_999
 
     expect(
       tracker.shouldSuppressIOSSendMessagePush({
